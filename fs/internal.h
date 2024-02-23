097072aabd222626ea337d764735241509a07ada
Celica Sylphil <kamikitakomaric@protonmail.com>
2024-02-23 17:49:14 +0800

    Add umount code for KernelSU
    Signed-off-by: Ruchit <risenid@duck.com>

--- fs/internal.h
+++ fs/internal.h
@@ -97,6 +97,8 @@ extern void __init mnt_init(void);
 extern int __mnt_want_write_file(struct file *);
 extern void __mnt_drop_write_file(struct file *);
 
+int path_umount(struct path *path, int flags);
+
 extern void dissolve_on_fput(struct vfsmount *);
 /*
  * fs_struct.c
